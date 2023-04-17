afb9fe66168350ba63973e2caecdd8c2628e7482
Harriet Oteng <harriet.oteng@gmail.com>
2023-04-17 14:11:34 +0000

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import './mystyles.css'
+import './mystyles.css';
 
-const logoDiv = document.querySelector('.logo-div')
+import companyLogo from '../Assets/company_logo.png';
 
-import company_logo from '../Assets/company_logo.png'
+const logoDiv = document.querySelector('.logo-div');
 const component = () => {
   const element = document.createElement('div');
-  const mycompany_logo = new Image();
-  mycompany_logo.src = company_logo;
-  mycompany_logo.className = 'logo';
-  element.appendChild(mycompany_logo);
+  const mycompanyLogo = new Image();
+  mycompanyLogo.src = companyLogo;
+  mycompanyLogo.className = 'logo';
+  element.appendChild(mycompanyLogo);
   return element;
-}  
+};
 logoDiv.appendChild(component());
