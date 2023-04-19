f0172a22a71bfd51ea9465fee325746f5a62ddbb
Dammy <bestbynature2010@gmail.com>
2023-04-19 13:26:33 +0100

    :hammer: Fix linter errors

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -1,15 +1,13 @@
-
-
 const database = (itemArr) => {
-    const newlist = itemArr.slice(0, 16);
-    return newlist.length
-  };
+  const newlist = itemArr.slice(0, 16);
+  return newlist.length;
+};
 
-  const commentsFunction = (...args) => {
-    const commentData = args[0]
-      const { length } = commentData;
-      return length;
-  };
+const commentsFunction = (...args) => {
+  const commentData = args[0];
+  const { length } = commentData;
+  return length;
+};
 
-  exports.database = database
-  exports.commentsFunction = commentsFunction
+exports.database = database;
+exports.commentsFunction = commentsFunction;
--- src/tests/moviecount.test.js
+++ src/tests/moviecount.test.js
@@ -3,22 +3,26 @@ jest.mock('../index.js');
 const { database } = require('../index.js');
 
 test('sample testing of jest', () => {
-    expect(1).toBe(1);
-  });
+  expect(1).toBe(1);
+});
 
-  test('Getting the movie count', () => {
-    let a = [
-        {
-            movieName: 'Jackie',
-            movieId: 1,
-            numberOfSeasons: 3
-        },
-        {movieName: 'Run for blood',
-            movieId: 2,
-            numberOfSeasons: 2},
-        {movieName: 'Prisoners',
-            movieId: 3,
-            numberOfSeasons: 6}]
+test('Getting the movie count', () => {
+  const a = [
+    {
+      movieName: 'Jackie',
+      movieId: 1,
+      numberOfSeasons: 3,
+    },
+    {
+      movieName: 'Run for blood',
+      movieId: 2,
+      numberOfSeasons: 2,
+    },
+    {
+      movieName: 'Prisoners',
+      movieId: 3,
+      numberOfSeasons: 6,
+    }];
 
-    expect(database(a)).toBe(a.length)
-  });
+  expect(database(a)).toBe(a.length);
+});
