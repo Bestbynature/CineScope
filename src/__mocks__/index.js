471556284c6efa53e450be984b77f9dac44bcd44
Harriet Oteng <harriet.oteng@gmail.com>
2023-04-19 11:01:44 +0000

    Remove unused code

--- src/__mocks__/index.js
+++ src/__mocks__/index.js
@@ -5,11 +5,8 @@ const database = (itemArr) => {
     return newlist.length
   };
 
-
   const commentsFunction = (...args) => {
     const commentData = args[0]
-    //   await commentPoster(args[0]);
-    //   const commentData = commentGetter(args[0]);
       const { length } = commentData;
       return length;
   };
